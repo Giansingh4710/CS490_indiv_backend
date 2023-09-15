b2180ac5cfdc30dad89329ae465155efd1aaffe8
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2023-09-14 20:08:47 -0400

    moved frontend to react

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ app.listen(1313)
 // https://sentry.io/answers/why-does-my-javascript-code-receive-a-no-access-control-allow-origin-header-error-while-postman-does-not/
 const cors = require('cors')
 const corsOptions = {
-  origin: 'http://127.0.0.1:8080',
+  origin: 'http://localhost:3000',
 }
 app.use(cors(corsOptions))
 
 app.get('/test', (req, res) => {
   res.json({
-    msg: 'Hello World!',
+    msg: 'Hello',
   })
-  console.log('Hello World! Sent from server')
+  console.log('Hello Sent from server')
   // connection.query('select * from actor', (err, rows) => {
   //   if (err) throw err
   //   for (let i = 0; i < rows.length; i++) {
